defa8a6d3057bb66aceb44a700108714269741a8
Narmin <nerminsuleyma6@gmail.com>
2024-08-11 01:35:13 +0400

    changes

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -5,5 +5,5 @@ using DataAccess.Concrete;
 using EntityLayer.Concrete;
 
 TravelManager travelManager = new(new EfTravelDal(new BaseProjectContext()));
-
+//
 Travel travel = new() { DepartureDate = DateTime.Now,Description="dxds",Destination="dcsdsd",Title="dcdc",IsDeleted=false };
